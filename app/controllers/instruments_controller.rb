c6f66b916ea6c1f3aa404e59c7b224e5f7448c25
Jonathan Oliveira <jonathanoliveirasilva9@gmail.com>
2020-09-11 22:43:08 -0300

    action show

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -5,4 +5,11 @@ class InstrumentsController < ApplicationController
       format.html
     end
   end
+
+  def show
+    @instrument = Instrument.find(params[:id])
+    respond_to do |format|
+      format.html
+    end
+  end
 end
--- spec/controllers/instruments_controller_spec.rb
+++ spec/controllers/instruments_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe InstrumentsController, type: :controller do
-  describe 'GET index' do
+  describe 'GET #index' do
     let(:instruments) { create_list(:instrument, 2) }
     before { get :index }
     it `assigns @instruments` do
@@ -14,6 +14,14 @@ RSpec.describe InstrumentsController, type: :controller do
 
     it `render the :index template` do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:instrument) { create(:instrument) }
+    before { get :show, params: { id: instrument.id } }
+    it `has 200 status code` do
+      expect(response).to have_http_status(:ok)
     end
   end
 end
