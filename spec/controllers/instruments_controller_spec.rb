64c311b5537299c6d91a038e71a617eac4e135ab
Jonathan Oliveira <jonathanoliveirasilva9@gmail.com>
2020-09-11 22:38:26 -0300

    finish test index

--- spec/controllers/instruments_controller_spec.rb
+++ spec/controllers/instruments_controller_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe InstrumentsController, type: :controller do
   describe 'GET index' do
-    it 'assigns @instruments' do
-      instrument = create(:instrument)
-      get :index
-      expect(assigns(:instruments)).to eq([instrument])
+    let(:instruments) { create_list(:instrument, 2) }
+    before { get :index }
+    it `assigns @instruments` do
+      expect(assigns(:instruments)).to match_array instruments
+    end
+
+    it `has 200 status code` do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it `render the :index template` do
+      expect(response).to render_template :index
     end
   end
 end
