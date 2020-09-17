8d1c3abf3f11a116713a9c548471bc71bde388c3
Jonathan Oliveira <jonathanoliveirasilva9@gmail.com>
2020-09-16 23:49:26 -0300

    add model brand

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_001831) do
+ActiveRecord::Schema.define(version: 2020_09_17_023539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string "fantasy_name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "instruments", force: :cascade do |t|
     t.string "serie"
---
+++ spec/factories/brands.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :brand do
+    fantasy_name { "MyString" }
+    description { "MyText" }
+  end
+end
