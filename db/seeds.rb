f61b863493377299c137d8e94d233e5d6805eaa9
Jonathan Oliveira <jonathanoliveirasilva9@gmail.com>
2020-09-19 19:09:26 -0300

    add seed to master

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Instrument.destroy_all
+Brand.destroy_all
+
+brands = []
+15.times do |_|
+  brands << Brand.create(
+  fantasy_name:  Faker::JapaneseMedia::SwordArtOnline.game_name,
+  description:  Faker::Kpop.solo)
+end
+
+200.times do |_|
+  Instrument.create(
+    serie: Faker::Music.instrument,
+    price: Faker::Number.number(digits: 10),
+    line: Faker::Music::PearlJam.musician,
+    description: Faker::Lorem.paragraph,
+    brand_id: brands.sample.id)
+end
+
+
