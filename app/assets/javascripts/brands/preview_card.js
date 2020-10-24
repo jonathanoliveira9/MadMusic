1594fdb8412b05c5bcc96621085034c142a65c8b
Jonathan Oliveira <jonathanoliveirasilva9@gmail.com>
2020-10-24 09:54:52 -0300

    update preview

--- app/assets/javascripts/brands/preview_card.js
+++ app/assets/javascripts/brands/preview_card.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+  $('#default').show();
   $('li').filter('.list-group-item').click(function(){
     $.get(`brands/${this.value}`)
+    $('#default').hide();
   })
 })
